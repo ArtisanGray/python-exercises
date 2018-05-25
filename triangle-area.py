c2b7db9e217c80fe7e251e5f18e0f49414d9bd40
Elias Gray <kitwithshorts@gmail.com>
2018-05-25 08:26:09 -0500

    Create triangle-area.py
    prints the area of a triangle.

---
+++ triangle-area.py
@@ -0,0 +1,8 @@
+# Read the numbers b and h like this:
+b = int(input())
+h = int(input())
+#inputs for base and height value and then sends them to the print statement.
+
+
+print((b*h)/2)
+# Print the result with print()
