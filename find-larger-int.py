96ca514fe73c0839b1e3a18426107d23075956cc
Elias Gray <kitwithshorts@gmail.com>
2018-05-25 08:29:09 -0500

    Create find-larger-int.py

---
+++ find-larger-int.py
@@ -0,0 +1,13 @@
+# Read 2 int
+numA = input()
+numB = input()
+#<<By Ethan Greene>>	
+# Check to see which one is bigger
+if (numA < numB):
+     print('the bigger number is ' + numB)
+elif (numA > numB):
+     print('the bigger number is ' + numA)
+elif (numA == numB):
+     print('These numbers are equal.')
+
+# print the bigger integer
