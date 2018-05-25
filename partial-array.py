4ab4f4a5841f43932276d80f657b768575405f2e
Elias Gray <kitwithshorts@gmail.com>
2018-05-25 08:33:03 -0500

    Create partial-array.py
    returns elements of an array that are less than 30.

---
+++ partial-array.py
@@ -0,0 +1,7 @@
+# Given is the array below.
+a1 = [1, 1, 2, 3, 4, 5, 8, 11, 13, 17, 19, 20, 21, 34, 43, 55, 89]
+for scan in range(len(a1)):
+	if (a1[scan] < 30):
+		print(a1[scan])
+
+#Print out all elements of the array that are less than 30
