5d8fb3cdf1e7ab8d06a357ae7da04cebd2c49894
Elias Gray <kitwithshorts@gmail.com>
2018-05-25 08:30:32 -0500

    Create print_array.py
    prints out an array.

---
+++ print_array.py
@@ -0,0 +1,13 @@
+# This is an array that contains different foods
+food_items = ["turkey","ham", "spam","eggs","nuts"]
+print(food_items[3])
+# print "eggs" from the loop USING an Array
+for index in food_items:
+	print(index)
+
+	
+
+# Use a for loop to print all of the food items
+# Hint:  for x in food_items:
+  
+  
