46d2372dd45e9f6ce4dd6e4d942187e5b9516399
Elias Gray <kitwithshorts@gmail.com>
2018-05-29 20:33:57 -0500

    Create slicing-strings.py
    prints a series of print statements containing a substring or slice of the given string.

---
+++ slicing-strings.py
@@ -0,0 +1,19 @@
+# Read a string:
+
+# s = input()
+given = str(input())
+print(given[2:3])
+print(given[len(given)-2:len(given)-1])
+print(given[0:5])
+print(given[0: len(given)-2])
+print(given[::2])#even numbers start at 0, thanks Stack Overflow!
+print(given[1::2])
+print(given[::-1])
+print(given[::-2])
+print(len(given))
+
+
+
+# Print a string:
+
+# print(s)
