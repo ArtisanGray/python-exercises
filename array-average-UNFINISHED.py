d3945c40b2db8585ce331db775e3e4ae659dc562
Elias Gray <kitwithshorts@gmail.com>
2018-05-29 20:46:35 -0500

    Create array-average-UNFINISHED.py
    never completed.

---
+++ array-average-UNFINISHED.py
@@ -0,0 +1,11 @@
+# input how many arrays
+num_arrays = int(input("How many arrays do you want to find the average of?"))
+
+#Hint:
+a = input().split()
+a = [int(x) for x in a]
+
+print(a)
+
+#Now use the code to see how many arrays?
+#use a loop to now find the average and print the average.
