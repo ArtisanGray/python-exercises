1abbc2471f29192cb81416c2989cae244859197b
Elias Gray <kitwithshorts@gmail.com>
2018-05-29 20:34:57 -0500

    Create dictionaries.py
    Makes a dictionary?

---
+++ dictionaries.py
@@ -0,0 +1,28 @@
+# Create a mapping of state to abbreviation
+# Add Oklahoma and Texas to the end of the list.
+states = {'Oregon': 'OR',
+          'Florida': 'FL',
+          'California': 'CA',
+          'New York': 'NY',
+          'Michigan': 'MI'}
+          
+cities = {'CA': 'San Francisco',
+          'MI': 'Detroit', 
+          'FL': 'Jacksonville'}  
+          
+#add the rest of the cities here including Oklahoma and Texas
+
+cities ['NY'] = 'New York'
+cities ['OR'] = 'Portland'
+states['OK']='Oklahoma'
+states['TX']='Texas'
+# Print out all the cities
+print ('-' * 20)
+print (cities)
+
+
+# Print out all the states
+print ('-' * 20)
+print (states)
+
+
